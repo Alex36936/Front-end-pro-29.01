9ec96cb8b08b01dba128f205626f171510cc5d05
Alexa36936 <vakulenkosasha1995@gmail.com>
2024-02-04 00:25:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//First Task - homework
+let userNickname='Nick';
+let userScore=25;
+
+//Second Task
+let price = prompt('Ціна покупки');
+let quantity = prompt('кількість продуктів');
+
+console.log('price', price);
+console.log('quantity', quantity);
+
+let summOperation = Number(price*quantity);
+console.log(summOperation);
+
+//Third Task
+let sale = Number((summOperation*5)/100);
+console.log(sale);
+
